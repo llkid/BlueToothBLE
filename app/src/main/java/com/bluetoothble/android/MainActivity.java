4db56486b5119bdee971d8d6e01fdbbe394577b5
shi <3114045651@qq.com>
2020-02-01 21:12:17 +0800

    分支提交测试2

--- app/src/main/java/com/bluetoothble/android/MainActivity.java
+++ app/src/main/java/com/bluetoothble/android/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "execute here test");
+        Log.d("MainActivity", "execute here test");
     }
 }
